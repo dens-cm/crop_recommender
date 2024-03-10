4ca52140dadeef0dce775afe2db03f45ae3bde17
Dens.CM <144193048+dens-cm@users.noreply.github.com>
2024-03-10 18:51:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request, jsonify
 import joblib
 import numpy as np
-import os
+from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 
-#load the trained model
+#load the trained model and scaler
 knn_model = joblib.load('knn_model.joblib')
+scaler = joblib.load('model_scaler.joblib')
 
 @app.route('/')
 def index():
@@ -21,18 +22,16 @@ def predict():
                         float(input_data['pH']), float(input_data['temp']), float(input_data['humid']),
                         float(input_data['rainfall'])]
 
-        # Perform the prediction using the loaded KNN model
-        prediction = knn_model.predict([input_values])[0]
-
-        # Convert the NumPy integer to a native Python integer
-        prediction = int(prediction)
+        # Perform the prediction using the loaded KNN model and the loaded scaler
+        scaled_inputs = scaler.transform([input_values])
+        prediction = int(knn_model.predict(scaled_inputs)[0])
 
         # Return the prediction as JSON
         return jsonify({'crop': prediction})
-    
+
     except Exception as e:
         print('Error:', str(e))
         return jsonify({'error': 'An error occurred during prediction.'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
