c5f97acb81513de35dd4c667610426ec59edc537
Dens.CM <144193048+dens-cm@users.noreply.github.com>
2024-03-10 13:55:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 app = Flask(__name__)
 
 #load the trained model
-knn_model = joblib.load('knn_model2.joblib')
+knn_model = joblib.load('/model/knn_model3.joblib')
 
 @app.route('/')
 def index():
